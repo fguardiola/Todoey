6713afd5462af6952178c760d6eca9b6094e4c9d
fguardiola <fernando.guardiola.888@gmail.com>
2018-09-28 12:16:59 +0200

    Add new item functionality completed

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    let todoData = ["Llamar coxinilla","Concertar cita","Disfrutar de la vida"];
+    var todoData = ["Llamar coxinilla","Concertar cita","Disfrutar de la vida"];
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,6 +33,27 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    @IBAction func addButtonPressed(_ sender: Any) {
+        //We need to have access to the textfield that is local to alert.addTextField . We need to give access to that var otside
+        var accesibleTextField = UITextField() //create an object
+        
+        let alert = UIAlertController(title: "Add a new item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            //Handle user clicks
+            if (accesibleTextField.text?.count)! > 0 {
+                self.todoData.append(accesibleTextField.text!)
+                //even if you have added the item it s not going to be diplayed unless you reload the table
+                self.tableView.reloadData()
+            }
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "New todoey"
+            accesibleTextField = alertTextField // Objects are references so we are pointing to the same obj
+//            print(alertTextField.text)
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
     }
 }
 
